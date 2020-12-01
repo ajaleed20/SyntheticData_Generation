0ac42e83e428e4e19def0012237162c15f1de557
ajaleed20 <70899365+ajaleed20@users.noreply.github.com>
2020-12-01 20:49:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import easygui
 import tkinter as tk
 from datetime import datetime, date, time, timedelta
 import time
@@ -8,13 +9,17 @@ import collections
 import statistics
 import sklearn
 import math
-
+from sklearn import metrics
 from matplotlib import pyplot
 from pandas import read_csv
 from sklearn.utils import shuffle
 import os
 import ctypes
-from util.enums import GanualityLevel
+from keras.layers import Input, Dense, Dropout, concatenate, Conv1D, MaxPooling1D, Flatten, AveragePooling1D
+from keras.models import Model
+from sklearn.model_selection import train_test_split
+from util.enums import GranularityLevel,InputEnums
+#from input_lag_lead import input_lead_lag
 
 
 
@@ -114,31 +119,31 @@ def printAnswers(answers):
 
     if answers['Granularity'] == 'd':
         SyntheticValuesCount = deltadate.days
-        level = GanualityLevel.one_day.value
+        level = GranularityLevel.one_day.value
 
     elif answers['Granularity'] == 'h':
         SyntheticValuesCount = (deltadate.days) * 24
-        level = GanualityLevel.one_hour.value
+        level = GranularityLevel.one_hour.value
 
     elif answers['Granularity'] == 'm':
         SyntheticValuesCount = (deltadate.days) * 24 * 60
-        level = GanualityLevel.one_minute.value
+        level = GranularityLevel.one_minute.value
 
     elif answers['Granularity'] == 's':
         SyntheticValuesCount = (deltadate.days) * 24 * 60 * 60
-        level = GanualityLevel.one_sec.value
+        level = GranularityLevel.one_sec.value
 
     elif answers['Granularity'] == 'tm':
         SyntheticValuesCount = (deltadate.days) * 24 * 6
-        level = GanualityLevel.ten_min.value
+        level = GranularityLevel.ten_min.value
 
     elif answers['Granularity'] == 'ts':
         SyntheticValuesCount = (deltadate.days) * ((24 * 60) * 2)
-        level = GanualityLevel.thirty_sec.value
+        level = GranularityLevel.thirty_sec.value
 
     elif answers['Granularity'] == 'w':
         SyntheticValuesCount = (deltadate.days) / 7
-        level = GanualityLevel.one_week.value
+        level = GranularityLevel.one_week.value
 
     else:
         print("Granularity entered wrong: Rerun Code with following entry:\n\n"+
@@ -203,35 +208,254 @@ def printAnswers(answers):
     # print([item for item, count in collections.Counter(df).items() if count > 1])
 
     df['ts'] = pd.DataFrame({'ts': pd.date_range(start=sdate, end=edate, freq=get_freq_by_level(level))})
+
+
+    df['ts'] = pd.to_datetime(df['ts'])
+    df['ts'] = pd.to_datetime(df['ts']).dt.tz_localize(None)
     df = df.sort_values(by='ts')
     df.to_csv(answers['FileName'] + '.csv', index=False)
     print(df)
-    #
-    # series = read_csv(answers['FileName'] + '.csv', header=0, index_col=0)
-    # print(series.shape)
+
+    series = read_csv(answers['FileName'] + '.csv', header=0, index_col=1)
+    print(series.shape)
+
+    #print(series)
+    #X[0, :], X[1, :], c = y, cmap = plt.cm.Spectral
     # pyplot.plot(series)
     # pyplot.show()
-    # print("end")
+    #
+    # input_lead_lag()
+    # lead_time, lag_time = send_values(lead=0,lag=0)
+
+    # choice = easygui.enterbox("Enter Machine Learning Model choice:\n 1:MultiLayerPerceptron\n 2:Convolutional Neural Network\n 3:CNN_with_HyperParameterTuning\n 4:MLP_with_HyperParameterTuning\n Default:MultiLayerPerceptron")
+    # # choice = input("Enter choice:\n 1:mlp\n 2:cnn\n 3:cnn_hp\n 4:mlp_hp")
+    # print("User Choice is: " + choice)
+
+
+
+    features = get_features(InputEnums.input.value)
+    data = get_data(series,sdate,edate,level)
+    forecast_input = get_forecast_input(data['Feature'],features, InputEnums.lag_time_steps.value)
+    #data = series_to_supervised(series, 3)
+
+    x_inputs, x_outputs, testX, testY, trainY = prepare_data_mlp(data['Feature'], features, InputEnums.lag_time_steps.value, InputEnums.lead_time_steps.value, InputEnums.test_train_split_size.value)
+
+    model = get_mlp_model(x_inputs, InputEnums.lag_time_steps.value, InputEnums.lead_time_steps.value)
+
+    forecast, model, rmse_list, rmse = get_learning_curve_and_forecast(model, x_inputs, x_outputs, trainY, testX,
+                                                                       testY,
+                                                                       InputEnums.lead_time_steps.value,
+                                                                       InputEnums.lag_time_steps.value, features, forecast_input,
+                                                                       InputEnums.confidence_interval_multiple_factor.value)
+
+    print("end")
 
     #series = read_csv('monthly-car-sales.csv', header=0, index_col=0)
 
 
+
+
+def get_mlp_model(x_inputs, lag_time_steps, lead_time_steps):
+    input_models = []
+    dense_layers = []
+    for i in range(len(x_inputs)):
+        visible = Input(shape=(lag_time_steps,))
+        hidden0 = Dense(200, activation='relu')(visible)  # sigmoid #tanh #relu
+        dropout0 = Dropout(0.5)(hidden0)
+        hidden1 = Dense(100, activation='relu')(dropout0)  # sigmoid #tanh #relu
+        dropout1 = Dropout(0.5)(hidden1)
+        dense = Dense(50, activation='relu')(dropout1)  # relu #sigmoid #tanh
+        input_models.append(visible)
+        dense_layers.append(dense)
+
+    if len(x_inputs) > 1:
+        merge = concatenate(dense_layers)
+    else:
+        merge = dense_layers[0]
+
+    hidden1 = Dense(len(x_inputs) * 32, activation='relu')(merge)  # sigmoid #tanh #relu
+    dropout1 = Dropout(0.1)(hidden1)
+    hidden2 = Dense(len(x_inputs) * 16, activation='relu')(dropout1)  # relu #sigmoid #tanh
+    dropout2 = Dropout(0.5)(hidden2)
+    hidden3 = Dense(len(x_inputs), activation='relu')(dropout2)  # relu #sigmoid #tanh
+    dropout3 = Dropout(0.1)(hidden3)
+    output = Dense(lead_time_steps)(dropout3)
+
+    model = Model(inputs=input_models, outputs=output)
+
+    model.compile(optimizer='adam', loss='mse')
+    return model
+
+
+def prepare_data_mlp(data, features, lag_time_steps, lead_time_steps, test_split_size):
+    X, y = series_to_supervised(data, lag_time_steps, lead_time_steps)
+
+    leadColumns = [col for col in y.columns] #if target_mp_id == col[0:col.find('(')]]
+    y = y[leadColumns]
+    values = X.values
+    X = values.reshape((values.shape[0], lag_time_steps, len(features)))
+    trainX, testX, trainY, testY = train_test_split(X, y, test_size=test_split_size, shuffle=False)
+
+    x_inputs = []
+    x_outputs = []
+
+    for i in range(trainX.shape[2]):
+        x_inputs.append(trainX[:, :, i-1])
+    for i in range(testX.shape[2]):
+        x_outputs.append(testX[:, :, i-1])
+
+    return x_inputs, x_outputs, testX, testY, trainY
+
+
+
+
+def get_learning_curve_and_forecast(model, x_inputs, x_outputs, trainY, testX, testY, lead_time_step, lag_time_steps,
+                                    features, forecast_input, confidence_interval_multiple_factor ,normalize_data= False,  normalizer= None, power_transformers= None):
+
+        history = model.fit(x_inputs, trainY, epochs=100, verbose=2, validation_data=(x_outputs, testY))
+        # plt.plot(history.history['loss'], label='train')
+        # plt.plot(history.history['val_loss'], label='test')
+        # plt.legend()
+        # plt.savefig('Graphs/'+'specialties_ '+'_learning curve_Lag_' + str(lag_time_steps) + '_Lead_' + str(lead_time_step) + '_iteration')
+        # plt.show()
+        forecast = model.predict(forecast_input)
+
+        rmse, rmse_list = cal_rmse(model, x_outputs, testX, testY, lead_time_step,lag_time_steps, features, normalizer,
+                                   power_transformers, normalize_data=False)
+        rmse_list = [rmse * confidence_interval_multiple_factor for rmse in rmse_list]
+
+        return forecast, model, rmse_list,rmse
+
+
+def get_features(var):
+    return list(map(str, var))
+
+
+def get_forecast_input(data,features , lag_time_steps):
+    X, y = series_to_supervised(data, lag_time_steps, 0)
+    values = X.values
+    X = values.reshape((values.shape[0], lag_time_steps, len(features)))
+    x_inputs = []
+
+    for i in range(X.shape[2]):
+        x_inputs.append(np.reshape(X[-1, :, i-1], (1, X.shape[1])))
+    return x_inputs
+
+
+def cal_rmse(model, x_outputs, testX, testY, lead_time_steps,
+             lag_time_steps, features, normalizer, power_transformers, normalize_data=True):
+    forecast = model.predict(x_outputs)
+    testY_transformed = testY.values
+
+    if normalize_data:
+        tempTestX = testX.reshape(testX.shape[0], lag_time_steps * len(features))[:,
+                        -(len(features)):-1]
+        if len(tempTestX.shape) == 1:
+            tempTestX = np.reshape(tempTestX, (1, tempTestX.shape[0]))
+
+        for i in range(lead_time_steps):
+            raw_forecast = inverse_transform_forecast(forecast[:, i], tempTestX, normalizer, power_transformers,
+                                                          features)
+            forecast[:, i] = raw_forecast.reshape(1, -1)
+
+        for i in range(lead_time_steps):
+            raw_test_data = inverse_transform_forecast(testY_transformed[:, i], tempTestX, normalizer, power_transformers,
+                                                          features)
+            testY_transformed[:, i] = raw_test_data.reshape(1, -1)
+
+    rmse, rmse_list = helper_service.rmse_time_series(testY_transformed, forecast)
+
+    return rmse, rmse_list
+
+
+def rmse_time_series(y_true, y_pred):
+    rmses = []
+    for i in range(y_true.shape[1]):
+        rmse = np.sqrt(metrics.mean_squared_error(y_true[:,i], y_pred[:,i]))
+        rmses.append(rmse)
+    return sum(rmses)/len(rmses), rmses
+
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    data = pd.DataFrame(data)
+    n_vars = 1 if type(data) is list else data.shape[1]
+    columns = data.columns
+    df = pd.DataFrame(data)
+    cols, leadNames, lagNames = list(), list(), list()
+
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        lagNames += [(columns[j] + '(t-%d)' % (i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        leadNames += [(columns[j] + '(t+%d)' % (i)) for j in range(n_vars)]
+
+    res = pd.concat(cols, axis=1)
+    res.columns = np.concatenate((lagNames, leadNames))
+
+    # drop rows with NaN values
+    if dropnan:
+        res.dropna(inplace=True)
+
+    return res[lagNames], res[leadNames]
+
+
+def get_data(df, start_period, end_period, granularity_level = GranularityLevel.one_hour.value[0],immute= True, drop_nan= True):
+    df.sort_values(by=['ts'], inplace=True)
+    res_df = df.drop_duplicates().reset_index(drop=True)
+
+    if immute:
+        res_df = res_df.fillna(method='ffill')
+    if drop_nan:
+        res_df.dropna(inplace=True)
+
+        return res_df
+
+
+
+
+def send_values(lead,lag):
+    return lead,lag
+#
+# def train_test_split(data, n_test):
+# 	return data[:-n_test], data[-n_test:]
+
+#
+#
+# def 1_series_to_supervised(data, n_in=1, n_out=1):
+# 	df = DataFrame(data)
+# 	cols = list()
+# 	# input sequence (t-n, ... t-1)
+# 	for i in range(n_in, 0, -1):
+# 		cols.append(df.shift(i))
+# 	# forecast sequence (t, t+1, ... t+n)
+# 	for i in range(0, n_out):
+# 		cols.append(df.shift(-i))
+# 	# put it all together
+# 	agg = concat(cols, axis=1)
+# 	# drop rows with NaN values
+# 	agg.dropna(inplace=True)
+# 	return agg.values
+
+
 def get_freq_by_level(ganuality_level_value):
-    if GanualityLevel.one_hour.value[0] == ganuality_level_value[0]:
+    if GranularityLevel.one_hour.value[0] == ganuality_level_value[0]:
         return '60T'
-    elif GanualityLevel.three_hour.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.three_hour.value[0] == ganuality_level_value[0]:
         return '180T'
-    elif GanualityLevel.one_day.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.one_day.value[0] == ganuality_level_value[0]:
         return '1440T'
-    elif GanualityLevel.one_week.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.one_week.value[0] == ganuality_level_value[0]:
         return '10080T'
-    elif GanualityLevel.ten_min.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.ten_min.value[0] == ganuality_level_value[0]:
         return '10T'
-    elif GanualityLevel.one_minute.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.one_minute.value[0] == ganuality_level_value[0]:
         return '1T'
-    elif GanualityLevel.one_sec.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.one_sec.value[0] == ganuality_level_value[0]:
         return '0.016666666667T'
-    elif GanualityLevel.thirty_sec.value[0] == ganuality_level_value[0]:
+    elif GranularityLevel.thirty_sec.value[0] == ganuality_level_value[0]:
         return '0.5T'
 
 
